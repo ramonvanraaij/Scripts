24a21694354f1215329ce0723e29de1a5e0ccb50
Rámon van Raaij <noreply@vanraaij.eu>
2024-10-20 12:11:04 +0200

    install_qnap-qsync.sh, removed install dependencies
    Removed unnecessary dependencies during
    installation if the PKGINFO wasn't corrected.

--- linux/Arch Linux/install_qnap-qsync.sh
+++ linux/Arch Linux/install_qnap-qsync.sh
@@ -38,7 +38,7 @@ debtap QNAPQsyncClientUbuntux64.deb
 
 # Install the converted package
 echo "Installing Qsync..."
-sudo pacman -U qnapqsyncclient*.pkg.tar.zst
+sudo pacman -U qnapqsyncclient*.pkg.tar.zst --assume-installed android-emulator --assume-installed anaconda --assume-installed plex-media-server --assume-installed activitywatch-bin --assume-installed cura-bin --assume-installed clion
 
 # Remove downloaded deb package (optional)
 rm -f QNAPQsyncClientUbuntux64.deb
